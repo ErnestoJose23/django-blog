4c5ba2699c200e44f26cdb3039b8d4d948f1ae73
Ernesto Custodio <ernestojosecg@gmail.com>
2020-05-28 11:28:20 +0200

    Views and Urls

--- Django/views.py
+++ Django/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<h1>Hola que tal</h1>")
+    return render(request, "index.html")
 
 def about_page(request):
     return HttpResponse("<h1>About us</h1>")
