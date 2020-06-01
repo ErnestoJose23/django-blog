4f0a56faad5fb251dba106764b6dc8398ccc988b
Ernesto Custodio <ernestojosecg@gmail.com>
2020-06-01 12:21:24 +0200

    Urls

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,13 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import BlogPost
 
-
+#Get = 1 object
+#Filter = [] objects
 def blog_post_detail_page(request, slug):
-    obj = get_object_or_404(BlogPost, slug= slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count() == 0:  
+        raise Http404
+    obj = queryset.first()
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
 
