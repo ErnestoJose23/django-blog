82b960e3a1e840b8b1b89cbd54e893c8a504b160
Ernesto Custodio <ernestojosecg@gmail.com>
2020-06-02 11:32:51 +0200

    Crud

--- Django/urls.py
+++ Django/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path #url
 from blog.views import (
-    blog_post_detail_page
+    blog_post_detail_page,
+    blog_post_list_view,
 )
 
 from .views import (home_page, about_page, contact_page)
@@ -25,6 +26,7 @@ from .views import (home_page, about_page, contact_page)
 urlpatterns = [
     path('', home_page),
     path('blog/<str:slug>/', blog_post_detail_page),
+    path('blog/', blog_post_list_view),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('contact/', contact_page),
