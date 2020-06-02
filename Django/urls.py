7aa930cf1d0a26fa7001421c716e4e2058e877a0
Ernesto Custodio <ernestojosecg@gmail.com>
2020-06-02 11:22:49 +0200

    Crud

--- Django/urls.py
+++ Django/urls.py
@@ -23,13 +23,10 @@ from blog.views import (
 from .views import (home_page, about_page, contact_page)
 
 urlpatterns = [
-    
     path('', home_page),
     path('blog/<str:slug>/', blog_post_detail_page),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
-    
-
 ]
