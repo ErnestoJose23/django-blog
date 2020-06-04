4a463f3e8a963734db3b6801b7a5bc8af3447a0d
Ernesto Custodio <ernestojosecg@gmail.com>
2020-06-04 12:05:38 +0200

    Validation Forms

--- Django/forms.py
+++ Django/forms.py
@@ -5,3 +5,11 @@ class ContactForm(forms.Form):
     full_name = forms.CharField()
     email = forms.EmailField()
     contect = forms.CharField(widget=forms.Textarea)
+
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        print(email)
+        if email.endswith(".edu")
+            raise forms.ValidationError("This is not a valid email, dont use .edu")
+        return email
