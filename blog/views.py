95f3c3cb1cf602a6580ab37814d86b156e0378b1
Ernesto Custodio <ernestojosecg@gmail.com>
2020-06-04 11:57:00 +0200

    Blog Form

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,10 @@ from .models import BlogPost
 def blog_post_create_view(request):
     form = BlogPostForm(request.POST or None)
     if form.is_valid():
-        print(form.cleaned_data)
+        obj = BlogPost.objects.create(**form.cleaned_data)
+        form = BlogPostForm()
     template_name = 'contact.html'
-    context = {'form': None}
+    context = {'form': form}
     return render(request, template_name, context)
 
 def blog_post_list_view(request):
