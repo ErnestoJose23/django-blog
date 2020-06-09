1820c4cc82199181cdebcef16e021902d65f56e9
Ernesto Custodio <ernestojosecg@gmail.com>
2020-06-09 11:30:53 +0200

    NavigatioN

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,11 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+
+    def get_edit_url(self):
+        return f"/blog/{self.slug}/edit"
+        
