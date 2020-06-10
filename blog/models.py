b33ab6b47ecf85b0e6386bc232815565cf0c8067
Ernesto Custodio <ernestojosecg@gmail.com>
2020-06-10 11:27:15 +0200

    Publish Date

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,9 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField( auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
     def get_absolute_url(self):
